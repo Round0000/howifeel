08414c60a1d003ac3e29544898d33d1263246e3b
Round0000 <billybinche@gmail.com>
2022-02-11 00:26:46 +0100

    commit

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-ui_form.addEventListener("submit", (e) => {
-  e.preventDefault();
+// ui_form.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  if (e.target.evaluation.value.length === 0) return;
+//   if (e.target.evaluation.value.length === 0) return;
 
-  const obj = {};
+//   const obj = {};
 
-  [obj.evaluation, obj.comment] = [
-    e.target.evaluation.value,
-    e.target.comment.value,
-  ];
+//   [obj.evaluation, obj.comment] = [
+//     e.target.evaluation.value,
+//     e.target.comment.value,
+//   ];
 
-  console.table(obj);
-  updateUIstats(obj);
+//   console.table(obj);
+//   updateUIstats(obj);
+// });
+
+// function updateUIstats(obj) {
+//   const list = ui_stats.querySelector("ul");
+//   const item = document.createElement("LI");
+//   item.innerText = `10/02 - ${obj.evaluation}`;
+//   list.append(item);
+// }
+
+slider.addEventListener("change", (e) => {
+  console.log(e.target.value);
 });
-
-function updateUIstats(obj) {
-  const list = ui_stats.querySelector("ul");
-  const item = document.createElement("LI");
-  item.innerText = `10/02 - ${obj.evaluation}`;
-  list.append(item);
-}
