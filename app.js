2a135e75680a663e2127402464feafdb147cf920
Round0000 <billybinche@gmail.com>
2022-02-11 16:35:35 +0100

    commit

--- app.js
+++ app.js
@@ -73,6 +73,21 @@ charts_style_options.addEventListener("click", (e) => {
   }
 });
 
+document.body.innerHTML = navigator.platform;
+
+function iOS() {
+  return [
+    'iPad Simulator',
+    'iPhone Simulator',
+    'iPod Simulator',
+    'iPad',
+    'iPhone',
+    'iPod'
+  ].includes(navigator.platform)
+  // iPad on iOS 13 detection
+  || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+}
+
 reset_app.addEventListener("click", (e) => {
   slider.value = 50;
   container.classList.remove("move", "movehigher");
